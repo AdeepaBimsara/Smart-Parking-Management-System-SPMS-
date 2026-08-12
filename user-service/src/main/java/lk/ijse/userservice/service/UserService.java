5759a3ad51e85f6747a2d64d6be13c024157cc6c
Adeepa_Bimsara <adeepabimsara6@gmail.com>
2026-08-12 18:30:50 +0530

    create user service

---
+++ user-service/src/main/java/lk/ijse/userservice/service/UserService.java
@@ -0,0 +1,16 @@
+package lk.ijse.userservice.service;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * ============================================================
+ * Project      : Smart-Parking-Management-System
+ * Author      : Adeepa Bimsara
+ * GitHub      : https://github.com/AdeepaBimsara
+ * Created     : 8/12/26 6:30 PM
+ * ============================================================
+ * ============================================================
+ */
+@Service
+public class UserService {
+}
